7eddc8dd968293cdf8d5f1cccdd343597622ee14
srozb <github@rozbicki.eu>
2017-08-04 08:57:18 +0200

    another jse-decoder fix

--- processing/jse_decoder.py
+++ processing/jse_decoder.py
@@ -6,12 +6,12 @@ from fame.common.utils import tempdir
 class JSE_Decode(ProcessingModule):
     name = "jse-decode"
     description = "Decode JScript.Encoded file"
-    acts_on = "data"
 
     def each(self, target):
         if not target.lower().endswith('.jse'):
             return False
         tmpdir = tempdir()
-        os.system('./decoder {} {}/{}'.format(target, tmpdir, target[:-1]))
+        os.system('fame/modules/srozb/processing/decoder {} {}/{}'.format(
+            target, tmpdir, target[:-1]))
         self.add_extracted_file("{}/{}".format(tmpdir, target[:-1]))
         return True
