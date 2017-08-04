0f17a25d352bd2ef79b44f3ac18620c9bcb7d35d
srozb <github@rozbicki.eu>
2017-08-04 08:11:15 +0200

    fix

--- processing/ace_extract.py
+++ processing/ace_extract.py
@@ -12,19 +12,18 @@ class ACE_Extract(ProcessingModule):
         arch_ext = ['.rar', '.zip', '.arj']
         return target[-4:].lower() in arch_ext and magic.from_file(target).startswith('ACE')
 
-    def extract(self, target, tempdir):
-        tempdir = tempdir()
-        os.system("acefile-unace -d {} -x {}".format(tempdir, target))
+    def extract(self, target):
+        tmpdir = tempdir()
+        os.system("acefile-unace -d {} -x {}".format(tmpdir, target))
         files = os.popen("acefile-unace -l {}".format(target)).read().split('\n')
         for i in range(len(files)):
-            files[i] = tempdir + '/' + files[i]
+            files[i] = tmpdir + '/' + files[i]
         return files
 
     def each(self, target):
-        tempdir = tempdir()
         if self.is_susp_ace(target):
             self.add_tag('ACE_disguised')
-            files = self.extract(target, tempdir)
+            files = self.extract(target)
             for f in files:
                 self.add_extracted_file(f)
             return True
